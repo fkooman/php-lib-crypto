9c7c6a04866cc254e2015f805330669cacd787ee
François Kooman <fkooman@tuxed.net>
2015-07-10 22:13:43 +0200

    add some more tests

--- tests/fkooman/Crypto/CryptoTest.php
+++ tests/fkooman/Crypto/CryptoTest.php
@@ -18,6 +18,7 @@
 
 namespace fkooman\Crypto;
 
+use fkooman\Base64\Base64Url;
 use PHPUnit_Framework_TestCase;
 
 class CryptoTest extends PHPUnit_Framework_TestCase
@@ -30,9 +31,45 @@ class CryptoTest extends PHPUnit_Framework_TestCase
 
         $c = new Crypto($encryptSecret, $signSecret);
         $cipherText = $c->encrypt($plainText);
-        $this->assertEquals(
+        $this->assertSame(
             $plainText,
             $c->decrypt($cipherText)
         );
+    }
+
+    public function testDecrypt()
+    {
+        $c = new Crypto('bf76d65e841dcb5bb9e45a6e9027393d', '57d8eb862864f427d02f7364afe52732');
+        $cipherText = 'eyJpIjoiNGY2YzM2MTE0YjBkYTM2Nzg5MzYxNzA3YzQ2NDMyMDEiLCJjIjoiRUw0dG80bkVQMDRBYkdtV1dGR1wvOWc9PSIsIm0iOiJhZXMtMTI4LWNiYyIsImgiOiJzaGEyNTYifQ.b3mCkMDCoclvSVN5dIyHc9htW6AIsD4o9z35nYamrts';
+        $this->assertSame(
+            'Hello World',
+            $c->decrypt($cipherText)
+        );
+
+        $this->assertSame(
+            array(
+                'i' => '4f6c36114b0da36789361707c4643201',
+                'c' => 'EL4to4nEP04AbGmWWFG/9g==',
+                'm' => 'aes-128-cbc',
+                'h' => 'sha256',
+            ),
+            json_decode(
+                Base64Url::decode(
+                    explode('.', $cipherText)[0]
+                ),
+                true
+            )
+        );
+    }
+
+    /**
+     * @expectedException RuntimeException
+     * @expectedMessageException invalid signture
+     */
+    public function testInvalidSignature()
+    {
+        $c = new Crypto('bf76d65e841dcb5bb9e45a6e9027393d', '57d8eb862864f427d02f7364afe52732');
+        $cipherText = 'eyJpIjoiMjhkZTAyNzYyMmEzMzUzMjFhYTI1OGFlZDMxMzMxMDQiLCJjIjoidmZ3Y2ZHTituSEV1c0Z6UWpuZ2JyUT09IiwibSI6ImFlcy0xMjgtY2JjIiwiaCI6InNoYTI1NiJ9.b3mCkMDCoclvSVN5dIyHc9htW6AIsD4o9z35nYamrts';
+        $c->decrypt($cipherText);
     }
 }
